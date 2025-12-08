3891e8543acdd7dc87630ea198d47dcc61d1e481
Sebastien Chassande <chassande@gmail.com>
2025-12-08 16:52:35 +0100

    ajustement de la configuration

---
+++ lang/en/paygw_helloasso.php
@@ -0,0 +1,15 @@
+<?php
+$string['pluginname'] = 'HelloAsso';
+$string['clientid'] = 'Client ID';
+$string['clientid_desc'] = 'Your Client ID of the HelloAsso API';
+$string['clientsecret'] = 'Client Secret';
+$string['clientsecret_desc'] = 'Your Client Secret of the HelloAsso API';
+$string['org_slug'] = 'Organization slug';
+$string['org_slug_desc'] = 'Identifier of your HelloAsso organization';
+$string['paymentdesc'] = 'HelloAsso Payment';
+$string['formid'] = 'Form ID';
+$string['formid_desc'] = 'The ID of your HelloAsso payment form (see the URL)';
+$string['payment_success'] = 'Your payment was successfully processed via HelloAsso.';
+$string['payment_cancelled'] = 'Your payment was cancelled.';
+$string['paymentverificationfailed'] = 'Payment verification failed';
+$string['payment_error'] = 'An error occurred during payment processing';
--- version.php
+++ version.php
@@ -2,8 +2,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'payment_gateway_helloasso';
+$plugin->component = 'paygw_helloasso';
 $plugin->version = 2024061000;
 $plugin->requires = 2023042400;
-$plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.1 (alpha)';
+$plugin->maturity = MATURITY_RC;
+$plugin->release = '0.1 (release candidate)';
