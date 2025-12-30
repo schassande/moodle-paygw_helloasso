3c59337b4e33cf3cb315e04861b08e6d00e00fcb
Sebastien Chassande <chassande@gmail.com>
2025-12-30 14:41:47 +0100

    version 0.5.0

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'paygw_helloasso';
-$plugin->version = 2025122922;
+$plugin->version = 2025123000;
 $plugin->requires = 2023042400;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '0.4.3 (stable)';
+$plugin->release = '0.5.0 (stable)';
