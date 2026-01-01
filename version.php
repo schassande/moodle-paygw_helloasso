4724c205abd1f42731076afb772ed40035af1124
Sebastien Chassande <chassande@gmail.com>
2026-01-01 21:33:26 +0100

    version 0.6.0

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'paygw_helloasso';
-$plugin->version = 2025123000;
+$plugin->version = 2026010100;
 $plugin->requires = 2023042400;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '0.5.0 (stable)';
+$plugin->release = '0.6.0 (stable)';
